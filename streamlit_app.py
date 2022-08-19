7c9d404017a6b943708b5f268d8a3510029d886f
Juliana Velásquez Aguilar <90804320+JulianaVeAg@users.noreply.github.com>
2022-08-19 19:39:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents new Healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale, Spinach and rocket smoothie')
+streamlit.title('My parents new Healthy diner')
+
