599587d84a2dae832bd85db54c77177d478d9e70
Juliana Velásquez Aguilar <90804320+JulianaVeAg@users.noreply.github.com>
2022-08-20 00:35:01 +0200

    Update streamlit_app.py
    Añadimos un dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Crea tu propio batido de frutas 🥝🍇')
 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
