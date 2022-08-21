dd5e7f26477c15fbcf7d66b320ae210666f5f090
Juliana Velásquez Aguilar <90804320+JulianaVeAg@users.noreply.github.com>
2022-08-21 11:36:54 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show) 
 
 #Nueva sección para desplegar la respuesta de la api.
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 
 fruityvice_respnse=requests.get("https://www.fruityvice.com/api/fruit/watermelon")
